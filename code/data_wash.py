79f0e720a51e4aea5bbcb0fed4cfab1bab6beb93
ylf2002 <58096957+ylf2002@users.noreply.github.com>
2021-05-28 12:43:51 +0800

    Update data_wash.py

--- code/data_wash.py
+++ code/data_wash.py
@@ -7,7 +7,7 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel(r"E:\大二下\人工智能基础\crawl\lol\data\first_lol_role_data.xls")
+df = pd.read_excel(r"lol\data\first_lol_role_data.xls")
 
 ''' *—————— 处理一英雄多角色的第一种方法——将一列变多列 ——————* '''
 temp_list = [eval(x) for x in  df["职业"].tolist()]  
@@ -18,7 +18,7 @@ zeros_data = pd.DataFrame(np.zeros((df.shape[0], len(tag_all))), columns=list(ta
 for i in range(df.shape[0]):
     zeros_data.loc[i, temp_list[i]] = 1
 data = pd.concat([df, zeros_data], axis=1).drop(labels="职业", axis=1)
-data.to_csv("E:\\大二下\\人工智能基础\\crawl\\lol\\data\\clean_lol_role_data.csv",index=False)
+data.to_csv("lol\\data\\clean_lol_role_data.csv",index=False)
 
 
 ''' **—————— 处理一英雄多角色的第二种方法——将一行变多行 ——————** '''
@@ -34,7 +34,7 @@ df_new['职业'] = df_new['职业'].str.replace("[","")
 df_new['职业'] = df_new['职业'].str.replace("]","")
 # 替换
 df_new['职业'] = df_new['职业'].map(roles_mapping)
-df_new.to_csv("E:\\大二下\\人工智能基础\\crawl\\lol\\data\\clean_lol_role_data_length.csv",index=False)
+df_new.to_csv("lol\\data\\clean_lol_role_data_length.csv",index=False)
 
 
 ''' ***—————— 方便机器学习分析进行的数据清洗 ——————***'''
@@ -51,5 +51,5 @@ df_new = df_new.drop(columns = ['周免'])
 roles_mapping = {0:'fighter' ,1:'mage' , 2:'tank' ,3:'assassin' ,4:'support' ,5:'marksman'}
 # 替换
 df_new['职业'] = df_new['职业'].map(roles_mapping)
-df_new.to_csv("E:\\大二下\\人工智能基础\\crawl\\lol\\data\\roles.csv", index=False, header=False)
+df_new.to_csv("lol\\data\\roles.csv", index=False, header=False)
 
